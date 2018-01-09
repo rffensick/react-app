ebf49a383570e46c095d2bc02d6a30f0b4ac52dd
Eduard Ilyaskin <ilyaskin1998@gmail.com>
2018-01-10 00:33:45 +0600

    filter reduce

--- src/AC/index.js
+++ src/AC/index.js
@@ -1,27 +1,27 @@
 import { INCREMENT, DELETE_ARTICLE, FILTER_ARTICLE } from "./../constants";
 
 export function increment() {
-  return {
-    type: INCREMENT
-  }
+	return {
+		type: INCREMENT
+	}
 }
 
 export function deleteArticle(id) {
-  return {
-    type: DELETE_ARTICLE,
+	return {
+		type: DELETE_ARTICLE,
 
-    payload: {
-      id
-    }
-  }
+		payload: {
+			id
+		}
+	}
 }
 
 export function filterArticle(titleArticle) {
-  return {
-    type: FILTER_ARTICLE,
-    
-    payload: {
-      titleArticle
-    }
-  }
+	return {
+		type: FILTER_ARTICLE,
+		
+		payload: {
+			titleArticle
+		}
+	}
 }
