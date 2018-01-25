cc05c047bdf859a01f4bb8ea3177ba3da5d31616
Eduard Ilyaskin <ilyaskin1998@gmail.com>
2018-01-25 14:07:04 +0600

    123

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default class App extends Component {
       <div>
         <Counter />
         <FilterArticle articles={[]} />
-        <ArticlesList  />
+        <ArticlesList />
       </div>
     );
   }
