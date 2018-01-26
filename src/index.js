e6016afa2c611cd667fb9454a5845f4b0b6afac7
Eduard Ilyaskin <ilyaskin1998@gmail.com>
2018-01-26 19:12:11 +0600

    react-hot-loader added

--- src/index.js
+++ src/index.js
@@ -7,11 +7,24 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
+import { AppContainer } from "react-hot-loader";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, 
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    </AppContainer>,
   document.getElementById('root'));
+}
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  })
+}
   
 registerServiceWorker();
