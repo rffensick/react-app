43d3e5249a53cdf4381cdf8392f04ad0a1278727
Eduard Ilyaskin <ilyaskin1998@gmail.com>
2018-01-29 22:09:58 +0600

    123

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -2,7 +2,7 @@ import { START, SUCCESS, FAIL } from "../constants";
 
 export default store => next => action => {
 	const { callAPI, type, ...rest } = action;
-	console.log(...rest);
+	
 	if (!callAPI) return next(action);
 
 	next({
